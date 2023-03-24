3ff11c9fd914347d6e1412675ce161c45770c672
MaikelGevdLeeuw <maikelgevdleeuw@outlook.com>
2023-03-24 13:35:39 +0100

    Uitwerkingen

--- loops-arrays-objects.js
+++ loops-arrays-objects.js
@@ -1,6 +1,5 @@
 // ==========================================
-// Opdracht 1a
-// Schrijf een script dat voor iedere student in de array de score (het getal uit de property "score") in de terminal print
+
 console.log("----------");
 console.log("Opdracht 1a");
 const scores = [
@@ -14,29 +13,8 @@ for (let i = 0; i < scores.length; i++){
     console.log(scores[i].score);
 }
 
-
-// Verwachtte uitkomsten:
-// 83
-// 77
-// 92
-// 66ç
 // ==========================================
 
-
-
-
-
-// ==========================================
-// Opdracht 1b
-// Breid je script uit door voor iedere student de score om te rekenen naar een letter en dit in de terminal te printen
-// < 60 = F, < 70 = D, <80 = C, <90 B, <100 = A
-// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
-// Verwachtte uitkomsten:
-// B
-// C
-// A
-// D
-// ==========================================
 console.log("----------");
 console.log("Opdracht 1b");
 for (let i = 0; i < scores.length; i++){
@@ -56,17 +34,7 @@ for (let i = 0; i < scores.length; i++){
 }
 
 // ==========================================
-// Opdracht 1c
-// Breid je script uit door de bijbehorende letter op te slaan in de 'grade'-property van ieder student-object in de array.
-// Vóór het script zie je de originele objecten,
-// Na jouw script zie je de aangepaste objecten:
-// console.log(scores) geeft: [
-//  { name: 'Max', score: 83, grade: 'B' },
-//  { name: 'David', score: 77, grade: 'C' },
-//  { name: 'Khalid', score: 92, grade: 'A' },
-//  { name: 'Rianne', score: 66, grade: 'D' }
-//  ];
-// ==========================================
+
 console.log("----------");
 console.log("Opdracht 1c");
 for (let i = 0; i < scores.length; i++){
@@ -87,8 +55,7 @@ for (let i = 0; i < scores.length; i++){
 console.log(scores);
 
 // ==========================================
-// Opdracht 2
-// Schrijf een script die e-mailadressen genereert voor al onze medewerkers. Sla dit op in een nieuwe property "email" die je toevoegt aan iedere medewerker.
+
 console.log("----------");
 console.log("Opdracht 2");
 const NOVIEmployees = [
@@ -98,25 +65,12 @@ const NOVIEmployees = [
     {firstName: 'Mark', lastName: 'Rensen'},
 ];
 
-
 for (let i = 0; i < NOVIEmployees.length; i++){
     NOVIEmployees[i].email = NOVIEmployees[i].firstName + NOVIEmployees[i].lastName + "@novi.nl";
 }
 console.log(NOVIEmployees);
-// Bij NOVI horen de e-mailadressen altijd in het volgende format: voornaam.achternaam@novi.nl
-// Vóór het script zie je de originele objecten,
-// Na jouw script zie je de aangepaste objecten:
-// console.log(NOVIEmployees) geeft: [
-//   { firstName: 'Nova', lastName: 'Eeken', email: 'Nova.Eeken@novi.nl' },
-//   { firstName: 'Sam', lastName: 'Barnhoorn', email: 'Sam.Barnhoorn@novi.nl' },
-//   { firstName: 'Tessa', lastName: 'Steur', email: 'Tessa.Steur@novi.nl' },
-//   { firstName: 'Mark', lastName: 'Rensen', email: 'Mark.Rensen@novi.nl' },
-//  ];
-// ==========================================
 
 
-// Opdracht 2-BONUS
-// Lukt het je om ervoor te zorgen dat alle e-mailadressen in lowercase letters komen te staan? Dit heb je nog niet geleerd, maar google is your best friend...
 // ==========================================
 console.log("----------");
 console.log("Opdracht 2 - Bonus");
@@ -126,16 +80,6 @@ for (let i = 0; i < NOVIEmployees.length; i++){
 console.log(NOVIEmployees);
 
 // ==========================================
-// Opdracht 3
-// Schrijf een script die de property "neighborhood" (de buurt) invult op basis van de postcode van onze studenten.
-// Tip: is het hier handig om if-else te gebruiken, of is er wellicht een betere manier?
-// 	3513 = Pijlsweerd
-//  3514 = Vogelenbuurt
-//  3512 = Binnenstad
-//  3531 = Lombok
-//  3572 = Wittevrouwen
-//  3581 = Oudwijk
-//  3583 = Schildersbuurt
 
 const students = [
     {name: 'Mariska', city: 'Utrecht', zipCode: '3513', neighborhood: null},
@@ -182,21 +126,6 @@ for (let i = 0; i < students.length; i++){
     }
 }
 console.log(students)
-// Verwachte uitkomsten:
-// [
-//     { name: 'Mariska', city: 'Utrecht', zipCode: '3513', neighborhood: 'Pijlsweerd' },
-//     { name: 'Mehmet', city: 'Utrecht', zipCode: '3514', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Dennis', city: 'Utrecht', zipCode: '3572', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Robin', city: 'Utrecht', zipCode: '3581', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Tanush', city: 'Utrecht', zipCode: '3512', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Florien', city: 'Utrecht', zipCode: '3513', neighborhood: 'Pijlsweerd' },
-//     { name: 'Larissa', city: 'Utrecht', zipCode: '3583', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Marijn', city: 'Utrecht', zipCode: '3572', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Jan', city: 'Utrecht', zipCode: '3531', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Laura', city: 'Utrecht', zipCode: '3531', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Aicha', city: 'Utrecht', zipCode: '3514', neighborhood: 'Vogelenbuurt' },
-//     { name: 'Karima', city: 'Utrecht', zipCode: '3531', neighborhood: 'Vogelenbuurt' }
-// ]
 // ==========================================
 
 
